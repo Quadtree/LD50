7c5fb6fcf8c58457d1d2ae8367aa25e26a05935c
Quadtree <quadtree@gmail.com>
2022-04-02 14:09:43 -0700

    U

---
+++ ui/Minimap.cs
@@ -0,0 +1,64 @@
+using System;
+using Godot;
+using Godot.Collections;
+
+public class Minimap : Control
+{
+    [Export]
+    Texture PlanetTexture;
+
+    [Export]
+    Texture SunTexture;
+
+    [Export]
+    Texture ShipTexture;
+
+    Dictionary<Spatial, TextureRect> Objects = new Dictionary<Spatial, TextureRect>();
+
+    // Called when the node enters the scene tree for the first time.
+    public override void _Ready()
+    {
+
+    }
+
+    // Called every frame. 'delta' is the elapsed time since the previous frame.
+    public override void _Process(float delta)
+    {
+        // TODO: Perf check this
+        foreach (var it in GetTree().CurrentScene.FindChildrenByType<Planet>()) AddIfNotThere(it);
+        foreach (var it in GetTree().CurrentScene.FindChildrenByType<Ship>()) AddIfNotThere(it);
+
+        var toDelete = new Array<Spatial>();
+
+        foreach (var it in Objects)
+        {
+            if (!it.Key.IsInstanceValid())
+            {
+                toDelete.Add(it.Key);
+            }
+            else
+            {
+                var pos = it.Key.GetGlobalLocation();
+                it.Value.RectPosition = new Vector2(pos.x + 150, pos.z + 150);
+            }
+        }
+    }
+
+    void AddIfNotThere(Spatial spatial)
+    {
+        if (!Objects.ContainsKey(spatial))
+        {
+            var tex = ShipTexture;
+
+            if (spatial is Planet && ((Planet)spatial).IsSun) tex = SunTexture;
+            if (spatial is Planet && !((Planet)spatial).IsSun) tex = PlanetTexture;
+
+            var rect = new TextureRect();
+            rect.Texture = tex;
+            rect.RectSize = new Vector2(24, 24);
+            AddChild(rect);
+
+            Objects[spatial] = rect;
+        }
+    }
+}
