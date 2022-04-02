f15dd7a9e0173a482caf1a0ef077f19287fd8247
Quadtree <quadtree@gmail.com>
2022-04-02 02:43:15 -0700

    Updated

--- actors/Ship.cs
+++ actors/Ship.cs
@@ -7,6 +7,8 @@ public class Ship : RigidBody
     // private int a = 2;
     // private string b = "text";
 
+    Vector2 Control;
+
     // Called when the node enters the scene tree for the first time.
     public override void _Ready()
     {
@@ -23,6 +25,14 @@ public class Ship : RigidBody
     {
         base._PhysicsProcess(delta);
 
-        ApplyImpulse(new Vector3(0, 0, 0), new Vector3(-1, 0, 0) * delta);
+        ApplyImpulse(new Vector3(0, 0, 0), new Vector3(Control.x, 0, Control.y) * delta);
+    }
+
+    public override void _Input(InputEvent @event)
+    {
+        base._Input(@event);
+
+        Control.x = @event.GetActionStrength("move_right") - @event.GetActionStrength("move_left");
+        Control.y = @event.GetActionStrength("move_down") - @event.GetActionStrength("move_up");
     }
 }
